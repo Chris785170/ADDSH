c3999164e4dddc182327263a9a4b7b89dd4ba763
root <root@DESKTOP-1TKS4MH.localdomain>
2023-05-17 12:25:17 +0930

    a

--- klargest.cpp
+++ klargest.cpp
@@ -1,13 +1,23 @@
 #include <vector>
 #include <cmath>
 #include <queue> 
+#include <iostream>
 using namespace std; 
 
 int kth_largest(vector<int> values, int k){
     priority_queue<int> queue (values.begin(), values.end());
-    for (int i=0; i<k; i++){
+    for (int i=0; i<k-1; i++){
         queue.pop();
     }
 
     return queue.top();
 }
+/*
+int main(){
+
+    std::vector<int> values;
+    values = {1,2,3,4,5,6};
+    std::cout<<kth_largest(values, 3);
+    return 0;
+}
+*/
